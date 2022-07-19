bdd40bb48b9f025bd9a13971e051d44c958f44ca
Carlos Marchal <carlos.marchal@pm.me>
2022-07-19 18:21:19 +0200

    added current working directory test in command runner

--- src/command_runner_test.ts
+++ src/command_runner_test.ts
@@ -1,6 +1,21 @@
-import { DefaultCommandRunner } from "./command_runner.ts";
+import {
+  assertEquals,
+  assertInstanceOf,
+  assertRejects,
+} from "https://deno.land/std@0.148.0/testing/asserts.ts";
+
+import {
+  CommandExecutionError,
+  DefaultCommandRunner,
+} from "./command_runner.ts";
 
 Deno.test("DefaultCommandRunner smoke test", async () => {
   const runner = new DefaultCommandRunner();
   await runner.run(".", ["sh", "-c", "echo 'Hello World!'"]);
+});
+
+Deno.test("DefaultCommandRunner runs in specified directory", async () => {
+  const runner = new DefaultCommandRunner();
+  const command = ["sh", "-c", '[ "$PWD" = "/tmp" ] && exit 0 || exit 1'];
+  await runner.run("/tmp", command);
 });
